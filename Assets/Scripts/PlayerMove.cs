85754f227b30ba35b5f62b71d735f58e57b6e9f9
Simon <Simon>
2015-07-29 16:52:07 -0400

    Update

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	Rigidbody move;
+	float y;
+	public Camera mainCamera;
+	Vector3 temp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		temp = Input.mousePosition;
+		temp.z = transform.position.z - mainCamera.transform.position.z;
+		transform.position = mainCamera.ScreenToWorldPoint (temp);
+
+		//y = Input.GetAxis ("Mouse X");
+		//Vector3 localForce = TransformD(new Vector3(9, 3, y);
+		//localForce.y = 0;
+		//move.AddForce(localForce);
+	}
+}
